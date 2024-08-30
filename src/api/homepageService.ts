faa6de39fbf7963372e0c202f28a17b0da5566b7
MM16z <76926089+MM16z@users.noreply.github.com>
2024-08-31 01:17:22 +0700

    [fix] - fix some api services

--- src/api/homepageService.ts
+++ src/api/homepageService.ts
@@ -11,62 +11,42 @@ export const homepageApiService = {
 };
 
 async function getAllPosts(params: AllPostsParams, userId: string | undefined) {
-    try {
-        const response = await apiService().get(`${HOMEPAGE_BASE_URL}/${params.offset}`,
-            {
-                params: {
-                    userId,
-                }
+    const response = await apiService().get(`${HOMEPAGE_BASE_URL}/${params.offset}`,
+        {
+            params: {
+                userId,
             }
-        );
-        return response.data;
-    } catch (error) {
-        console.error("Error in getAllPosts:", error);
-        throw new Error('Failed to get all posts');
-    }
+        }
+    );
+    return response.data;
 }
 
 async function updatePostLike(postId: number, actionType: "like" | "unlike") {
-    try {
-        const response = await apiService().post(`${HOMEPAGE_BASE_URL}/update_post_like`,
-            JSON.stringify({ postId, actionType }), {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        return response.data;
-    } catch (error) {
-        console.error("Error in updatePostLike:", error);
-        throw new Error('Failed to update post like');
-    }
+    const response = await apiService().post(`${HOMEPAGE_BASE_URL}/update_post_like`,
+        JSON.stringify({ postId, actionType }), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return response.data;
 }
 
 async function createComment(payload: { postId: number; commentContent: string }) {
-    try {
-        const response = await apiService().post(`${HOMEPAGE_BASE_URL}/create_comment`,
-            JSON.stringify(payload), {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        return response;
-    } catch (error) {
-        console.error("Error in createComment:", error);
-        throw new Error('Failed to create comment');
-    }
+    const response = await apiService().post(`${HOMEPAGE_BASE_URL}/create_comment`,
+        JSON.stringify(payload), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return response;
 }
 
 async function deleteComment(id: number) {
-    try {
-        const response = await apiService().post(`${HOMEPAGE_BASE_URL}/delete_comment`,
-            JSON.stringify({ commentId: id }), {
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        return response;
-    } catch (error) {
-        console.error("Error in deleteComment:", error);
-        throw new Error('Failed to delete comment');
-    }
+    const response = await apiService().post(`${HOMEPAGE_BASE_URL}/delete_comment`,
+        JSON.stringify({ commentId: id }), {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    return response;
 }
--- src/api/userDashboardService.ts
+++ src/api/userDashboardService.ts
@@ -14,47 +14,27 @@ export const dashBoardApiService = {
 };
 
 async function getAllUserPosts(params: any) {
-    try {
-        const response = await apiService().get(`${USER_DASHBOARD_BASE_URL}/get_user_posts`, { params: params });
-        return response.data;
-    } catch (error) {
-        console.error("Error in getAllUserPosts:", error);
-        throw new Error('Failed to get all user posts');
-    }
+    const response = await apiService().get(`${USER_DASHBOARD_BASE_URL}/get_user_posts`, { params: params });
+    return response.data;
 }
 
 async function createPost(payload: any) {
-    try {
-        const response = await apiService().post(`${USER_DASHBOARD_BASE_URL}/create_post`, JSON.stringify(payload), {
-            headers: HEADERS
-        });
-        return response;
-    } catch (error) {
-        console.error("Error in createPost:", error);
-        throw new Error('Failed to create post');
-    }
+    const response = await apiService().post(`${USER_DASHBOARD_BASE_URL}/create_post`, JSON.stringify(payload), {
+        headers: HEADERS
+    });
+    return response;
 }
 
 async function editPost(payload: any) {
-    try {
-        const response = await apiService().post(`${USER_DASHBOARD_BASE_URL}/update_post`, JSON.stringify(payload), {
-            headers: HEADERS
-        });
-        return response;
-    } catch (error) {
-        console.error("Error in editPost:", error);
-        throw new Error('Failed to edit post');
-    }
+    const response = await apiService().post(`${USER_DASHBOARD_BASE_URL}/update_post`, JSON.stringify(payload), {
+        headers: HEADERS
+    });
+    return response;
 }
 
 async function deletePost(payload: any) {
-    try {
-        const response = await apiService().post(`${USER_DASHBOARD_BASE_URL}/delete_post`, JSON.stringify(payload), {
-            headers: HEADERS
-        });
-        return response;
-    } catch (error) {
-        console.error("Error in deletePost:", error);
-        throw new Error('Failed to delete post');
-    }
+    const response = await apiService().post(`${USER_DASHBOARD_BASE_URL}/delete_post`, JSON.stringify(payload), {
+        headers: HEADERS
+    });
+    return response;
 }
